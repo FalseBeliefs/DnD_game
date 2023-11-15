0a279eb2ce01c5fcfcdb56ab41bb09daaca99969
ino <ino@DESKTOP-UQTROJA>
2023-11-15 18:21:02 +0000

    new

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+
+with open("sword.json", "r") as file:
+    data = json.load(file)
+
+print(data)
+Max_HP = data["player"]["maxHealth"]
+Current_HP = data["player"]["currentHealth"]
+Weapon_name = data["player"]["mainHandWeapon"]["name"]
+
+print(f"HP: {Current_HP}/{Max_HP}")
+print(f"Main hand: {Weapon_name}")
